73410425fd4893d53932481c00fef4256a3aafb0
Dennis Kiesel <kiesel.dennis@gmail.com>
2014-01-03 21:18:16 -0700

    added logic to detect when loading logs

--- FilezillaLog/Scripts/app/controllers/LogController.js
+++ FilezillaLog/Scripts/app/controllers/LogController.js
@@ -6,27 +6,36 @@
             $scope.endDate = new Date();
             $scope.beginDate = new Date();
             $scope.beginDate.setDate($scope.endDate.getDate() - 2);
+            $scope.loading = true;
+            $scope.noResults = false;
 
-            Log.GetLogs($scope.beginDate, $scope.endDate).$promise.then(
+            getLogs = function (beginDate, endDate) {
+                $scope.loading = true;
+                $scope.noResults = false;
+                Log.GetLogs(beginDate, endDate).$promise.then(
                 function (model) {
                     $scope.logs = model;
+                    $scope.loading = false;
+                    if ($scope.logs.length === 0) {
+                        $scope.noResults = true;
+                    }
+                    else {
+                        $scope.noResults = false;
+                    }
                 });
+            };
+
+            getLogs($scope.beginDate, $scope.endDate);
 
             $scope.$watch('endDate', function (newVal, oldVal) {
                 if (newVal !== oldVal) {
-                    Log.GetLogs($scope.beginDate, $scope.endDate).$promise.then(
-                        function (model) {
-                            $scope.logs = model;
-                        });
+                    getLogs($scope.beginDate, $scope.endDate);
                 }
             })
 
             $scope.$watch('beginDate', function (newVal, oldVal) {
                 if (newVal !== oldVal) {
-                    Log.GetLogs($scope.beginDate, $scope.endDate).$promise.then(
-                        function (model) {
-                            $scope.logs = model;
-                        });
+                    getLogs($scope.beginDate, $scope.endDate);
                 }
             })
 
